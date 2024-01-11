0299797090bb237d7f65fa723b83b662b2c89571
nextechnexus <nextechnexus.team@gmail.com>
2024-01-11 08:45:51 +0100

    commit

--- app/components/MenuCard.jsx
+++ app/components/MenuCard.jsx
@@ -3,15 +3,15 @@ import Link from "next/link"
 const MenuCard = ({title,category,price,img}) => {
   return (
     <Link href={'/detail'}>
-      <div className='w-[200px] '>
+      <div className='max-w-[150px] '>
           <div className='flex justify-center items-center w-full h-96'>
               <img src={img} alt="" className='w-full h-full' />
           </div>
           <div className='flex flex-col mt-3'>
-              <span className='text-[#c5c2c5]'>{category}</span>
-              <h2 className='text-2xl font-bold'>{title}</h2>
-              <span>{price}$</span>
-              <button className='bg-purple text-white py-2 px-4 rounded-xl mt-3 text-center'>Add to Cart</button>
+              <span className='text-[#c5c2c5] md:text-sm'>{category}</span>
+              <h2 className='text-xl font-bold md:text-base'>{title}</h2>
+              <span className='md:text-sm'>{price}$</span>
+              <button className='bg-purple text-white py-2 px-4 rounded-xl mt-3 text-center md:text-sm md:py-1 md:px-3 md:rounded-md'>Add to Cart</button>
           </div>
 
       </div>
--- app/menu/page.js
+++ app/menu/page.js
@@ -5,28 +5,28 @@ import CardMenu from '../components/MenuCard'
 const MainMenu = () => {
   return (
     <Layout>
-      <section className='px-20 py-10 bg-[#f3f1f6]'>
-          <div><h1 className='text-4xl'>Main Menu</h1></div>
+      <section className='px-20 md:px-3 py-10 md:py-5 bg-[#f3f1f6]'>
+          <div><h1 className='text-4xl md:text-xl'>Main Menu</h1></div>
           <div className=''>
             <div className='flex justify-between my-5'>
-              <span>Showing 1-12 of 23 results</span>
-                <span>Default Sorting</span>
+              <span className='md:text-xs'>Showing 1-12 of 23 results</span>
+                <span className='md:text-xs'>Default Sorting</span>
             </div>
             <div>
-              <div className='flex flex-wrap gap-4 justify-between py-10'>
-              <CardMenu title={"1 Liter Drinks"} category={'Soft Drinks'} img={'klipartz.com.png'} price={"3.00$"}/>
-              <CardMenu title={"1 Liter Drinks"} category={'Soft Drinks'} img={'klipartz.com.png'} price={"3.00$"}/>
-              <CardMenu title={"1 Liter Drinks"} category={'Soft Drinks'} img={'klipartz.com.png'} price={"3.00$"}/>
-              <CardMenu title={"1 Liter Drinks"} category={'Soft Drinks'} img={'klipartz.com.png'} price={"3.00$"}/>
-              <CardMenu title={"1 Liter Drinks"} category={'Soft Drinks'} img={'klipartz.com.png'} price={"3.00$"}/>
-              <CardMenu title={"1 Liter Drinks"} category={'Soft Drinks'} img={'klipartz.com.png'} price={"3.00$"}/>
-              <CardMenu title={"1 Liter Drinks"} category={'Soft Drinks'} img={'klipartz.com.png'} price={"3.00$"}/>
-              <CardMenu title={"1 Liter Drinks"} category={'Soft Drinks'} img={'klipartz.com.png'} price={"3.00$"}/>
-              <CardMenu title={"1 Liter Drinks"} category={'Soft Drinks'} img={'klipartz.com.png'} price={"3.00$"}/>
-              <CardMenu title={"1 Liter Drinks"} category={'Soft Drinks'} img={'klipartz.com.png'} price={"3.00$"}/>
-              <CardMenu title={"1 Liter Drinks"} category={'Soft Drinks'} img={'klipartz.com.png'} price={"3.00$"}/>
-              <CardMenu title={"1 Liter Drinks"} category={'Soft Drinks'} img={'klipartz.com.png'} price={"3.00$"}/>
-              <CardMenu title={"1 Liter Drinks"} category={'Soft Drinks'} img={'klipartz.com.png'} price={"3.00$"}/>
+              <div className='flex flex-wrap gap-5 justify-around items-center md:items-center py-10'>
+                <CardMenu title={"1 Liter Drinks"} category={'Soft Drinks'} img={'klipartz.com.png'} price={"3.00$"}/>
+                <CardMenu title={"1 Liter Drinks"} category={'Soft Drinks'} img={'klipartz.com.png'} price={"3.00$"}/>
+                <CardMenu title={"1 Liter Drinks"} category={'Soft Drinks'} img={'klipartz.com.png'} price={"3.00$"}/>
+                <CardMenu title={"1 Liter Drinks"} category={'Soft Drinks'} img={'klipartz.com.png'} price={"3.00$"}/>
+                <CardMenu title={"1 Liter Drinks"} category={'Soft Drinks'} img={'klipartz.com.png'} price={"3.00$"}/>
+                <CardMenu title={"1 Liter Drinks"} category={'Soft Drinks'} img={'klipartz.com.png'} price={"3.00$"}/>
+                <CardMenu title={"1 Liter Drinks"} category={'Soft Drinks'} img={'klipartz.com.png'} price={"3.00$"}/>
+                <CardMenu title={"1 Liter Drinks"} category={'Soft Drinks'} img={'klipartz.com.png'} price={"3.00$"}/>
+                <CardMenu title={"1 Liter Drinks"} category={'Soft Drinks'} img={'klipartz.com.png'} price={"3.00$"}/>
+                <CardMenu title={"1 Liter Drinks"} category={'Soft Drinks'} img={'klipartz.com.png'} price={"3.00$"}/>
+                <CardMenu title={"1 Liter Drinks"} category={'Soft Drinks'} img={'klipartz.com.png'} price={"3.00$"}/>
+                <CardMenu title={"1 Liter Drinks"} category={'Soft Drinks'} img={'klipartz.com.png'} price={"3.00$"}/>
+                <CardMenu title={"1 Liter Drinks"} category={'Soft Drinks'} img={'klipartz.com.png'} price={"3.00$"}/>
               <CardMenu title={"1 Liter Drinks"} category={'Soft Drinks'} img={'klipartz.com.png'} price={"3.00$"}/>
                 <CardMenu title={"1 Liter Drinks"} category={'Soft Drinks'} img={'klipartz.com.png'} price={"3.00$"}/>
               </div>
@@ -35,7 +35,7 @@ const MainMenu = () => {
             <div className='flex gap-x-3'>
               <div className='border-purple border py-2 px-3 bg-purple text-white font-bold'>1</div>
               <div className='border-purple border py-2 px-3'>2</div>
-              <div className='border-purple border py-2 px-3'>2</div>
+              <div className='border-purple border py-2 px-3'>3</div>
 
             </div>
 
